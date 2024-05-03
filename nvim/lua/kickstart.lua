f53f4163bb8d0b4427156b519c05b269ddaca4d5
rjacobus <roberto.jacobus.trasvina@intel.com>
2024-05-02 20:57:58 -0600

    Use go.vim instead of vim-go o7

--- nvim/lua/kickstart.lua
+++ nvim/lua/kickstart.lua
@@ -230,7 +230,23 @@ require("lazy").setup({
 	"junegunn/fzf.vim",
 	"quangnguyen30192/cmp-nvim-ultisnips",
 	"SirVer/ultisnips",
-	"fatih/vim-go",
+	--"fatih/vim-go",
+
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
+
 	"rafi/awesome-vim-colorschemes",
 	{ "akinsho/toggleterm.nvim", version = "*", config = true },
 	--'xolox/vim-misc',
