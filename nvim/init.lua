5167b1ff60fbc7be19208d2e55f4c4659b49a8c1
rjacobus <roberto.jacobus.trasvina@intel.com>
2024-05-02 19:50:23 -0600

    Customize lua stuff prior to BREAKING it

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require 'kickstart'
+require("kickstart")
 
 -- ************ NOTE ************************
 -- USEFUL THINGS TO REMEMBER ON THIS MIGRATION
@@ -12,12 +12,18 @@ local keymap_set = vim.keymap.set
 
 vim.opt.incsearch = false
 
-vim.g.UltiSnipsExpandTrigger = '<tab>'
-vim.g.UltiSnipsJumpForwardTrigger = '<tab>'
-vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'
-vim.g.UltiSnipsSnippetDirectories = { '~/ultisnips' }
+vim.g.UltiSnipsExpandTrigger = "<tab>"
+vim.g.UltiSnipsJumpForwardTrigger = "<c-l>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<c-k>"
+-- vim.g.UltiSnipsSnippetDirectories = { "~/ultisnips" }
 
-vim.cmd [[colorscheme PaperColor]]
+vim.g.completion_enable_snippet = "UltiSnips"
 
-keymap_set('n', '<F8>', '<cmd>mksession! vimsesh<cr> <cmd>wqa<cr>')
-keymap_set('n', '<leader>t', '<cmd>tabe<cr>')
+vim.cmd([[colorscheme PaperColor]])
+
+keymap_set("n", "<F8>", "<cmd>mksession! vimsesh<cr> <cmd>wqa<cr>")
+keymap_set("n", "<c-f>", "<cmd>Telescope find_files<cr>")
+
+keymap_set("n", "<leader>e", "<cmd>Explore<cr>")
+keymap_set("n", "<leader>t", "<cmd>tabe<cr>")
+keymap_set("n", "<leader>rg", "<cmd>Telescope live_grep<cr>")
